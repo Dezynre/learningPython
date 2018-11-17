7aa71f0042dd912966c31c22ce2da9c8ea76e274
Chacha Ian <chachaian1997@gmail.com>
2018-11-17 01:46:02 -0800

    initial commit

---
+++ project1.py
@@ -0,0 +1,14 @@
+def printer():
+  print("My name is Chacha")
+
+printer()
+
+def printloop():
+  for i in range(5,16):
+    print(i)
+
+printloop()
+
+a=input("Enter a value")
+b=input("Enter another value")
+print(int(a)+int(b))
